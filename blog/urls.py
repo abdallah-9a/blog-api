52f86b8807473dcb1970cfdff769130780198f52
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-14 14:30:35 +0300

    Refactor URL patterns for posts and categories to use RESTful endpoints

--- blog/urls.py
+++ blog/urls.py
@@ -7,26 +7,12 @@ from .views import (
 )
 
 urlpatterns = [
-    path("posts/", PostListView.as_view(), name="posts-list"),
-    path("posts/", PostListView.as_view(), name="add-post"),
-    path("posts/<int:pk>/", PostRetrieveUpdateDeleteView.as_view(), name="post-view"),
-    path("posts/<int:pk>/", PostRetrieveUpdateDeleteView.as_view(), name="update-post"),
-    path("posts/<int:pk>/", PostRetrieveUpdateDeleteView.as_view(), name="delete-post"),
-    path("categories/", CategoryListCreateView.as_view(), name="categories-list"),
-    path("categories/", CategoryListCreateView.as_view(), name="add-category"),
+    path("posts/", PostListView.as_view(), name="posts"),
+    path("posts/<int:pk>/", PostRetrieveUpdateDeleteView.as_view(), name="post-detail"),
+    path("categories/", CategoryListCreateView.as_view(), name="categories"),
     path(
         "categories/<int:pk>/",
         CategoryRetrieveUpdateDeleteView.as_view(),
-        name="category-view",
-    ),
-    path(
-        "categories/<int:pk>/",
-        CategoryRetrieveUpdateDeleteView.as_view(),
-        name="update-category",
-    ),
-    path(
-        "categories/<int:pk>/",
-        CategoryRetrieveUpdateDeleteView.as_view(),
-        name="delete-category",
+        name="category-detail",
     ),
 ]
