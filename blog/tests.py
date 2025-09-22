75259a9eae73bd23d957708ad149e620bc58ac23
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2025-09-22 11:23:07 +0300

    test: cover all blog urls

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import (
+    PostListView,
+    CategoryListCreateView,
+    PostRetrieveUpdateDeleteView,
+    CategoryRetrieveUpdateDeleteView,
+    CommentListCreateView,
+    CommentRetrieveUpdateDeleteView,
+)
 
 # Create your tests here.
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_list_posts_url(self):
+        url = reverse("posts")
+        self.assertEqual(resolve(url).func.view_class, PostListView)
+
+    def test_post_detail_url(self):
+        url = reverse("post-detail", args=[1])
+        self.assertEqual(resolve(url).func.view_class, PostRetrieveUpdateDeleteView)
+
+    def test_list_categories_url(self):
+        url = reverse("categories")
+        self.assertEqual(resolve(url).func.view_class, CategoryListCreateView)
+
+    def test_category_detail_url(self):
+        url = reverse("category-detail", args=[1])
+        self.assertEqual(resolve(url).func.view_class, CategoryRetrieveUpdateDeleteView)
+
+    def test_list_comments_url(self):
+        url = reverse("comments", args=[1])
+        self.assertEqual(resolve(url).func.view_class, CommentListCreateView)
+
+    def test_comment_detail_url(self):
+        url = reverse("comment-detail", args=[1])
+        self.assertEqual(resolve(url).func.view_class, CommentRetrieveUpdateDeleteView)
